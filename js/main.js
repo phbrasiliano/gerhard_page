ba27ab34060e4ef051dcd8f001b479113848ce4d
Phellipe Brasiliano <phbrasiliano@gmail.com>
2020-11-22 18:34:33 +0100

    finishes most basic page structure, with page selection and navbar finished

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
 $(document).ready(function(){
+    var language = "de";
 
-}
+    $("#homeNav").click(function(){
+      $(".navbar-nav li").removeClass("active");
+      $(this).addClass("active")
+      $(".content").fadeOut().promise().done(function(){
+        $("#home").fadeIn();
+      });
+    });
+
+    $("#bioNav").click(function(){
+      $(".navbar-nav li").removeClass("active");
+      $(this).addClass("active")
+      $(".content").fadeOut().promise().done(function(){
+        $("#bio-de").fadeIn();
+      });
+
+    });
+
+    $("#worksNav").click(function(){
+      $(".navbar-nav li").removeClass("active");
+      $(this).addClass("active")
+      $(".content").fadeOut().promise().done(function(){
+        $("#works").fadeIn();
+      });
+
+
+    });
+});
