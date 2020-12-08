769f38d13118be546eb769e9d295b04841764659
Phellipe Brasiliano <phbrasiliano@gmail.com>
2020-12-08 15:05:16 +0100

    implements the language swapping logic

--- js/main.js
+++ js/main.js
@@ -1,30 +1,57 @@
 $(document).ready(function(){
     var language = "de";
+    var content = "home"
+
 
     $("#homeNav").click(function(){
+      content = "home";
       $(".navbar-nav li").removeClass("active");
       $(this).addClass("active")
       $(".content").fadeOut().promise().done(function(){
-        $("#home").fadeIn();
+        $("." + content + "." + language).fadeIn();
       });
     });
 
     $("#bioNav").click(function(){
+      content = "bio";
       $(".navbar-nav li").removeClass("active");
       $(this).addClass("active")
       $(".content").fadeOut().promise().done(function(){
-        $("#bio-de").fadeIn();
+        $("." + content + "." + language).fadeIn();
       });
 
     });
 
     $("#worksNav").click(function(){
+      content = "works";
       $(".navbar-nav li").removeClass("active");
       $(this).addClass("active")
       $(".content").fadeOut().promise().done(function(){
-        $("#works").fadeIn();
+        $("." + content + "." + language).fadeIn();
       });
 
-
     });
+
+    $("#deButton").click(function(){
+      if(language != "de"){
+        language = "de";
+        $(this).addClass("active")
+        $(".content").fadeOut().promise().done(function(){
+          $("." + content + "." + language).fadeIn();
+        });
+      };
+    });
+
+
+    $("#enButton").click(function(){
+      if(language != "en"){
+        language = "en";
+        $(this).addClass("active")
+        $(".content").fadeOut().promise().done(function(){
+          $("." + content + "." + language).fadeIn();
+        });
+      };
+    });
+
+
 });
