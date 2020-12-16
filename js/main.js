fd2224a5a40730dc66420964da07c42ab6ecdbcb
Phellipe Brasiliano <phbrasiliano@gmail.com>
2020-12-16 22:57:19 +0100

    adds the table generator function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,42 @@
+
+// Generates the table with all of Gerhards compositions,
+// out of a json file.
+function tableGenerator(json){
+  len = Object.keys(json).length;
+  for (var i=1; i < len; i++) {
+
+      var row = $("<tr>").addClass();
+
+      row.append($("<td>").addClass("workNumber").text(i));
+
+      var nameInstruments = $("<td>").addClass("nameInstruments");
+      nameInstruments.append($("<p>").addClass("workName").text(json[i]['name']));
+
+      var instrumentsText = json[i]['instruments'].join(", ") + "."
+
+      nameInstruments.append($("<p>").addClass("workInstruments").text(instrumentsText));
+
+      row.append(nameInstruments);
+
+      row.append($("<td>").addClass("workDuration").text(json[i]['duration']));
+      row.append($("<td>").addClass("workDate").text(json[i]['date']));
+
+      var sample = $("<td>").addClass("sample")
+      if (json[i]['sample'] == 'yes'){
+        sample.text("yes")
+      };
+
+      row.append(sample)
+
+      var row2 = row.clone();
+      $("#worksTableDe").append(row)
+      $("#worksTableEn").append(row2)
+
+    }
+}
+
+
+
 $(document).ready(function(){
     var language = "de";
     var content = "home"
@@ -35,7 +74,8 @@ $(document).ready(function(){
     $("#deButton").click(function(){
       if(language != "de"){
         language = "de";
-        $(this).addClass("active")
+        $(this).addClass(language)
+        navLangSwitch("de");
         $(".content").fadeOut().promise().done(function(){
           $("." + content + "." + language).fadeIn();
         });
@@ -47,17 +87,12 @@ $(document).ready(function(){
       if(language != "en"){
         language = "en";
         $(this).addClass("active")
+        navLangSwitch(language);
         $(".content").fadeOut().promise().done(function(){
           $("." + content + "." + language).fadeIn();
         });
       };
     });
 
-
+    tableGenerator(songJson);
 });
-
-// Generates the table with all of Gerhards compositions,
-// out of a json file.
-function tableGenerator(){
-
-}
