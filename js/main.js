2aca756087f869afdc37ea389eaf7ee4bda568b1
PBR <phellipe.brasiliano@soundcertificate.com>
2022-12-13 15:38:45 +0100

    separetes the table generator code from main.js into its own file, adds it to the index file

--- js/main.js
+++ js/main.js
@@ -1,40 +1,3 @@
-
-// Generates the table with all of Gerhards compositions,
-// out of a json file.
-function tableGenerator(json){
-  len = Object.keys(json).length;
-  for (var i=1; i < len; i++) {
-
-      var row = $("<tr>").addClass();
-
-      row.append($("<td>").addClass("workNumber").text(i));
-
-      var nameInstruments = $("<td>").addClass("nameInstruments");
-      nameInstruments.append($("<p>").addClass("workName").text(json[i]['name']));
-
-      var instrumentsText = json[i]['instruments'].join(", ") + "."
-
-      nameInstruments.append($("<p>").addClass("workInstruments").text(instrumentsText));
-
-      row.append(nameInstruments);
-
-      row.append($("<td>").addClass("workDuration").text(json[i]['duration']));
-      row.append($("<td>").addClass("workDate").text(json[i]['date']));
-
-      var sample = $("<td>").addClass("sample")
-      if (json[i]['sample'] == 'yes'){
-        sample.html('<a href="' + '#' + '"> <img src="./images/play_circle.svg" class="play-button" width="50" height="30" alt=""></a>')
-      };
-      console.log(sample)
-      row.append(sample)
-
-      var row2 = row.clone();
-      $("#worksTableDe").append(row)
-      $("#worksTableEn").append(row2)
-
-    }
-}
-
 function navLangSwitch(lang){
   if (lang == "en") {
     $("#homeNav").text("Home");
@@ -54,7 +17,7 @@ $(document).ready(function(){
     var content = "home"
 
     navLangSwitch();
-    
+
     $("#homeNav").click(function(){
       content = "home";
       $(".nav-masthead a").removeClass("active");
